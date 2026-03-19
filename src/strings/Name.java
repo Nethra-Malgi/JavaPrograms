7b4d1f6d453b6926fdcb38f7609ac2917bd33d82
Nethra-M <nethramalgi429@gmail.com>
2026-03-19 19:54:43 +0530

    committing name java program...

---
+++ src/strings/Name.java
@@ -0,0 +1,22 @@
+package strings;
+
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String s="i am nethra";
+		
+		if(s.equalsIgnoreCase("nethra"))
+		{
+			System.out.println("match");
+		}
+		else
+		{
+			System.out.println("not match");
+		}
+
+	}
+
+}
